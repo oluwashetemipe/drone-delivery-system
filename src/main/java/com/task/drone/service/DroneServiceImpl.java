3ac87e528a23ab87bd9c419b8e0c578abe522941
Oluwashetemipe <oluwashetemipe@gmail.com>
2022-06-09 23:55:16 +0100

    commit

--- src/main/java/com/task/drone/service/DroneServiceImpl.java
+++ src/main/java/com/task/drone/service/DroneServiceImpl.java
@@ -1,4 +1,10 @@
 package com.task.drone.service;
 
+import com.task.drone.dtos.DroneRegistrationRequestdto;
+
 public class DroneServiceImpl implements DroneService {
+    public void registerDrone(DroneRegistrationRequestdto droneRegistrationRequestdto){
+        registerDrone(droneRegistrationRequestdto);
+
+    }
 }
