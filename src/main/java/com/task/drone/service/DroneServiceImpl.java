9864e23dca329b5bb7932a8fdf950861f14982d2
Oluwashetemipe <oluwashetemipe@gmail.com>
2022-06-10 23:59:57 +0100

    commit

--- src/main/java/com/task/drone/service/DroneServiceImpl.java
+++ src/main/java/com/task/drone/service/DroneServiceImpl.java
@@ -1,10 +1,12 @@
 package com.task.drone.service;
 
 import com.task.drone.dtos.DroneRegistrationRequestdto;
+import org.springframework.stereotype.Service;
 
+@Service
 public class DroneServiceImpl implements DroneService {
     public void registerDrone(DroneRegistrationRequestdto droneRegistrationRequestdto){
-        registerDrone(droneRegistrationRequestdto);
+        return DroneRepository.save(droneRegistrationRequestdto);
 
     }
 }
